a1382486f205f3655a5cd4104478be2c24137a87
hallgrimur1471 <hallgrimur1471@gmail.com>
2017-03-16 10:18:03 +0000

    Add game loop

--- js/asteroids.js
+++ js/asteroids.js
@@ -1,1 +1,14 @@
-asteroids.js
+"use strict";
+
+function requestPreloads() {
+  // todo: do some preloading here
+  preloadDone();
+}
+
+function preloadDone() {
+  // todo: maybe do some instanziation here
+  main.init();
+}
+
+// Kick it off
+requestPreloads();
---
+++ js/keys.js
@@ -0,0 +1,23 @@
+var keys = [];
+
+function handleKeydown(evt) {
+  keys[evt.keyCode] = true;
+}
+
+function handleKeyup(evt) {
+  keys[evt.keyCode] = false;
+}
+
+function eatKey(keyCode, commandObject) {
+  commandObject = commandObject || keys;
+  var isDown = commandObject[keyCode];
+  commandObject[keyCode] = false;
+  return isDown;
+}
+
+function keyCode(keyChar) {
+  return keyChar.charCodeAt(0);
+}
+
+window.addEventListener('keydown', handleKeydown);
+window.addEventListener('keyup', handleKeyup);
